2e3790532feb22cd86ad3dce5e0e746ea2527d3e
GabeDuMa0507 <113396438+GabeDuMa0507@users.noreply.github.com>
2022-10-22 12:47:46 -0500

    Ahora si abre

--- MyReproductorMultimedia/Form1.Designer.cs
+++ MyReproductorMultimedia/Form1.Designer.cs
@@ -46,7 +46,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(690, 425);
+            this.button1.Location = new System.Drawing.Point(670, 425);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(98, 37);
             this.button1.TabIndex = 2;
